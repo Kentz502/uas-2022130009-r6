1302925015ed25e265d15466df16bf9d3e4f82bd
Kentzie <onixd502@gmail.com>
2024-12-06 22:21:15 +0700

    22:21

--- app/Http/Controllers/RSIXController.php
+++ app/Http/Controllers/RSIXController.php
@@ -33,12 +33,13 @@ class RSIXController extends Controller
             (object) ['id' => 8, 'name' => 'MP7', 'description' => 'Description of Weapon 2'],
         ];
         $maps = [
-            (object) ['id' => 1, 'name' => 'Map 1', 'description' => 'Description of Map 1'],
-            (object) ['id' => 2, 'name' => 'Map 2', 'description' => 'Description of Map 2']
+            (object) ['id' => 1, 'name' => 'Club House', 'description' => 'This map depicts an assault on a biker gang clubhouse. Featuring a full bar, gaming tables and leather furniture, this mixed interior/exterior layout offers a host of interesting tactical possibilities for both attacker and defender.'],
+            (object) ['id' => 2, 'name' => 'Coastline', 'description' => 'Team Rainbow has been called to safeguard the seashore of Ibiza, Spain. Stationed on Ibiza’s rocky shore, course through Balearic ruins and navigate booming bars to neutralize the threat. Get ready for a surround flow of combat laced with chaotic rushes as you explore a paradise gone wrong.'],
+            (object) ['id' => 3, 'name' => 'Border', 'description' => 'Team Rainbow has been called in to assist the local law enforcement and regain control of the border. A mix of old and new buildings with open air pathways make this the most destructible and open planned map to date'],
         ];
         $playerStats = [
-            (object) ['id' => 1, 'name' => 'Player 1', 'stat' => 'Top Player Stats'],
-            (object) ['id' => 2, 'name' => 'Player 2', 'stat' => 'Top Player Stats']
+            (object) ['id' => 1, 'name' => 'Kenji', 'stat' => 'Top Player Stats'],
+            (object) ['id' => 2, 'name' => 'Onixd502', 'stat' => 'Top Player Stats']
         ];
 
         // Send data to the view
